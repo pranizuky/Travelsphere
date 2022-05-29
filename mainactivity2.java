ee3e6cdc6f0ae92afbf60db866f1e37dafa225b7
Sohammhatre10 <106436641+Sohammhatre10@users.noreply.github.com>
2022-05-29 10:53:15 +0530

    Update mainactivity2.java

--- mainactivity2.java
+++ mainactivity2.java
@@ -11,7 +11,6 @@ import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity {
     private Button button1;
-    private Button button2;
     private Button button3;
     private Button button4;
     private Button button5;
@@ -26,13 +25,38 @@ public class MainActivity2 extends AppCompatActivity {
         button4 = findViewById(R.id.uttarakhand);
         button5 = findViewById(R.id.Westbengal);
         Intent intent1 = new Intent(this, MainActivity3.class);
+        Intent intent2 = new Intent(this,MainActivity5.class);
+        Intent intent3 = new Intent (this, MainActivity7.class);
+        Intent intent4 = new Intent(this, MainActivity4.class);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity2.this, "Welcome to the MAHARASHTRA page", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity2.this, "Welcome to the Maharashtra", Toast.LENGTH_SHORT).show();
                 startActivity(intent1);
             }
         });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity2.this, "Welcome to Karnataka", Toast.LENGTH_SHORT).show();
+                startActivity(intent2);
+            }
+        });
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity2.this, "Welcome to Uttarakhand", Toast.LENGTH_SHORT).show();
+                startActivity(intent3);
+            }
+        });
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity2.this, "Welcome to West Bengal", Toast.LENGTH_SHORT).show();
+                startActivity(intent4);
+            }
+        });
+
 
     }
 }
