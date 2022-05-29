61f868dfbdd1bbda127f0cec13b65c61318fd5bd
pranizuky <106436202+pranizuky@users.noreply.github.com>
2022-05-28 17:41:51 -1200

    Create mainactivity18.java

---
+++ mainactivity18.java
@@ -0,0 +1,13 @@
+package com.example.travelsphere;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity18 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main18);
+    }
+}
